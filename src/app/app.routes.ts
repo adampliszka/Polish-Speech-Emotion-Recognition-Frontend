e44e2d55fc8f0699104c799f70129e0754f78c38
Adam Pliszka <a3plis@hotmail.com>
2025-05-25 20:34:14 +0200

    Final

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { EmotionComponent } from './emotion/emotion.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '**', component: EmotionComponent },
+  { path: '', component: EmotionComponent }
+];
